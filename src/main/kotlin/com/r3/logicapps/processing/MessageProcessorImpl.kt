12bf2d7acd64c19d1d0ea96865fd29104caeca10
Tommy Lillehagen <tommy.lillehagen@r3.com>
2019-04-11 13:37:24 +0100

    Fallback to known linear ID if needed

--- src/main/kotlin/com/r3/logicapps/processing/MessageProcessorImpl.kt
+++ src/main/kotlin/com/r3/logicapps/processing/MessageProcessorImpl.kt
@@ -3,27 +3,38 @@ package com.r3.logicapps.processing
 import com.r3.logicapps.BusRequest
 import com.r3.logicapps.BusResponse
 import com.r3.logicapps.Invocable
+import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.flows.FlowLogic
 
 open class MessageProcessorImpl(
     private val startFlowDelegate: (FlowLogic<*>) -> FlowInvocationResult
 ) : MessageProcessor {
     override fun invoke(message: BusRequest): BusResponse = when (message) {
-        is BusRequest.InvokeFlowWithoutInputStates -> processInvocationMessage(message.requestId, message, true)
-        is BusRequest.InvokeFlowWithInputStates -> processInvocationMessage(message.requestId, message, false)
+        is BusRequest.InvokeFlowWithoutInputStates -> processInvocationMessage(message.requestId, null, message, true)
+        is BusRequest.InvokeFlowWithInputStates -> processInvocationMessage(
+            message.requestId,
+            message.linearId,
+            message,
+            false
+        )
         is BusRequest.QueryFlowState -> {
             TODO("Handler for QueryFlowState not implemented")
         }
     }
 
-    private fun processInvocationMessage(requestId: String, invocable: Invocable, isNew: Boolean): BusResponse {
+    private fun processInvocationMessage(
+        requestId: String,
+        linearID: UniqueIdentifier?,
+        invocable: Invocable,
+        isNew: Boolean
+    ): BusResponse {
         return try {
             val flowLogic = deriveFlowLogic(invocable.workflowName, invocable.parameters)
             val result = startFlowDelegate(flowLogic)
             BusResponse.FlowOutput(
                 ingressType = invocable::class,
                 requestId = requestId,
-                linearId = result.linearId ?: error("Unable to derive linear ID after flow invocation"),
+                linearId = result.linearId ?: linearID ?: error("Unable to derive linear ID after flow invocation"),
                 fields = result.fields,
                 isNewContract = isNew
             )
@@ -31,7 +42,7 @@ open class MessageProcessorImpl(
             BusResponse.FlowError(
                 ingressType = invocable::class,
                 requestId = requestId,
-                linearId = null,
+                linearId = linearID,
                 exception = exception
             )
         }
